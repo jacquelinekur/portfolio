e09b5b3df88407e9d884f56dfafa7ca4c6368f2d
jacquelinekur <jacquelinekurniawan02@gmail.com>
2025-07-25 15:32:26 +0700

    contact page: add features

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,43 @@
 import React from 'react'
+import github_icon from '../assets/github-logo.png'
+import linkedin_icon from '../assets/linkedin.png'
 
 function Contact() {
   return (
     <div>
+      <h1>Contact Me</h1>
+      <form action="">
+        <div>
+            <label>Name</label>
+            <input type="text" placeholder="Your Name" />
+        </div>
+
+        <div>
+            <label>Email</label>
+            <input type="email" placeholder='your.email@example.com'/>
+        </div>
+
+        <div>
+            <label>Phone Number</label>
+            <input type="tel" placeholder="Your Phone Number" />
+        </div>
+
+        <div>
+            <label>Message</label>
+            <textarea></textarea>
+        </div>
+
+         <button>Send</button>
+      </form>
       <div>
-        <h2>Contact Information</h2>
+        <h2>My Contact Information</h2>
+        <p>Email</p>
         <p>jacquelinekurniawan02@gmail.com</p>
+      </div>
+      <div>
+        <h2>Connect With Me</h2>
+        <a href="https://github.com/jacquelinekur" target='_blank'><img src={github_icon}/></a>
+        <a href="http://www.linkedin.com/in/jacquelinekur" target='_blank'><img src={linkedin_icon} /></a>
       </div>
     </div>
   )
